880a70c1a0b6b317c323d30e7b491bf67da8154d
vCesar1mx <cesarvaz.e7@gmail.com>
2021-12-09 15:50:11 -0600

    TryCatch for IA, crash error resolve

--- addons/IA/rtext.js
+++ addons/IA/rtext.js
@@ -1,21 +1,25 @@
 const Discord = require("discord.js-12");
-const fetch = require('node-fetch');
-const Tesseract = require('tesseract.js');
+const fetch = require("node-fetch");
+const Tesseract = require("tesseract.js");
 module.exports = {
     name: "rtext",
     description: ".",
     async execute(message, args) {
-        const file = message.attachments.first().url;
-        console.dir(message.attachments)
-        if (!file) return console.log('No attached file found');
+        try {
+            if (!message.attachments.first().url) return console.log("No attached file found");
+            const file = message.attachments.first().url;
+            console.dir(message.attachments);
 
-        Tesseract.recognize(
-            '' + file,
-            'spa', { logger: m => console.log(m) }
-            //'eng', { logger: m => console.log(m) }
-        ).then(({ data: { text } }) => {
-            console.log(text);
-            message.reply(text);
-        })
-    }
-}
+            Tesseract.recognize(
+                "" + file,
+                "spa", { logger: (m) => console.log(m) }
+                //'eng', { logger: m => console.log(m) }
+            ).then(({ data: { text } }) => {
+                console.log(text);
+                message.reply(text);
+            });
+        } catch (error) {
+            message.reply('Debes usar el $rtext con una imagen adjunta!.')
+        }
+    },
+};
