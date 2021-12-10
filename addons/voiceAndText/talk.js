691eb629d6fe34b2c914a4dca387662037fb0da9
vCesar1mx <cesarvaz.e7@gmail.com>
2021-12-10 13:25:38 -0600

    Added IA for image to text and Add functions for module VAT, talk, play music

--- addons/voiceAndText/talk.js
+++ addons/voiceAndText/talk.js
@@ -5,26 +5,27 @@ module.exports = {
     async execute(message, args, client) {
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
-            const gTTS = require('gtts');
-            const text = args.slice(' ').join(' ')
-            var gtts = new gTTS('' + text, 'es');
-            gtts.save('./temp/voice/' + args[0] + '.mp3', function(err, result) {
-                if (err) { throw new Error(err) }
+            const gTTS = require("gtts");
+            const text = args.slice(" ").join(" ");
+            var gtts = new gTTS("" + text, "es");
+            gtts.save("./temp/voice/" + args[0] + ".mp3", function(err, result) {
+                if (err) {
+                    throw new Error(err);
+                }
 
-                const dispatcher = connection.play('./temp/voice/' + args[0] + '.mp3');
+                const dispatcher = connection.play("./temp/voice/" + args[0] + ".mp3");
 
-                dispatcher.on('start', () => {
-                    console.log('audio.mp3 is now playing!');
+                dispatcher.on("start", () => {
+                    //console.log("audio.mp3 is now playing!");
                 });
 
-                dispatcher.on('finish', () => {
-                    console.log('audio.mp3 has finished playing!');
+                dispatcher.on("finish", () => {
+                    //console.log("audio.mp3 has finished playing!");
                     connection.disconnect();
                 });
 
-                dispatcher.on('error', console.error);
+                dispatcher.on("error", console.error);
             });
         }
-
     },
 };
